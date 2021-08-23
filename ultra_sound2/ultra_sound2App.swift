44e88b6e23c56c7e2312266f4331810b50c0602e
Kirill <pavlov140700@mail.ru>
2021-08-23 22:49:31 +0300

    Initial Commit

---
+++ ultra_sound2/ultra_sound2App.swift
@@ -0,0 +1,17 @@
+//
+//  ultra_sound2App.swift
+//  ultra_sound2
+//
+//  Created by Кирилл on 23.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct ultra_sound2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
